ffb0f692c866159ada83b36b1a4909cfc0862ef4
Enspi <94926571+Enspi@users.noreply.github.com>
2022-03-29 16:39:12 +0500

    Update login.js

--- src/api/auth/login.js
+++ src/api/auth/login.js
@@ -1,6 +1,6 @@
 import { CallPost } from "api";
 
 export async function CallLogin({ email, password }) {
-    const res = await CallPost('/api/auth/login', { email, password })
+    const res = await CallPost('/api/auth/login/', { email, password })
     return res
 }
