3608c1cbe23aaf5b38c942e25edfe4176cb60a11
Enspi <94926571+Enspi@users.noreply.github.com>
2022-03-29 16:39:53 +0500

    Update verification.js

--- src/api/auth/verification.js
+++ src/api/auth/verification.js
@@ -1,13 +1,13 @@
 import { CallPost } from "api"
 
 export async function CallCodeVerification({ email, code }) {
-    const res = await CallPost("/api/auth/verify-email", {
+    const res = await CallPost("/api/auth/verify-email/", {
         user_email: email,
         code,
     })
     return res
 }
 export async function CallResendVerification(email) {
-    const res = await CallPost("/api/auth/resend-verification", { email })
+    const res = await CallPost("/api/auth/resend-verification/", { email })
     return res
 }
