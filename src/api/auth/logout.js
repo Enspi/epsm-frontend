eade9234bfe87981d1dd23150121326ec7f0a26e
Enspi <94926571+Enspi@users.noreply.github.com>
2022-03-29 16:39:27 +0500

    Update logout.js

--- src/api/auth/logout.js
+++ src/api/auth/logout.js
@@ -1,6 +1,6 @@
 import { CallGet } from "../index"
 
 export async function CallLogout() {
-  const res = await CallGet("/api/auth/logout")
+  const res = await CallGet("/api/auth/logout/")
   return res
 }
